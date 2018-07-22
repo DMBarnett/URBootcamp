b9b4b8d2cac881bd211fa784b9fb082d359a69cb
DMBarnett <davidmitchellbarnett@gmail.com>
2018-07-21 22:53:52 -0400

    Finished module 7
    Need to upload the .txt and fixed broken folder to Google

--- PREWORK_DMB/Module-7/Broken_and_Working_Code/WorkingCode/logic.js
+++ PREWORK_DMB/Module-7/Broken_and_Working_Code/WorkingCode/logic.js
@@ -1,8 +1,5 @@
-// When the factButton is clicked...
 $("#factButton").on("click", function() {
-	// We generate a random number between 0 and 4 (the number of facts in the catFactArray)
 	var number = Math.floor((Math.random() * catFactArray.length));
-	// We display the fact from the catFactArray that is in the random position we just generated.
 	$("#factText").text(catFactArray[number])
 })
 
